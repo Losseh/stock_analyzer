783b4ada2f2508180521992463a190fabd1716d2
Adrian Szymanski (ext.) <Adrian.Szymanski@partner.bmw.de>
2020-08-21 09:21:06 +0200

    adjustment for stooq data

--- main.py
+++ main.py
@@ -6,30 +6,38 @@ def slice(data, length, shift):
   start = shift
   end = shift + length
 
-  if end == 0:
-    return data[start:]
-  else:
-    return data[start:end]
+  return data[start:end]
 
 def bollinger(data, length, shift):
   array = slice(data, length, shift)
   stdev = np.std(array)
   avg = np.mean(array)
   
+  low = avg - 2*stdev
+  high = avg + 2*stdev
+  
   return {
 #    'date': data.index,
-    'low': avg - 2*stdev,
+    'low': low,
     'mean': avg,
-    'high': avg + 2*stdev
+    'high': high,
+    'pos': (data[0]-low) / (high-low)
   }
 
 wig40 = "MWIG40TR.PL"
 dax = "^DAX"
 sp500 = "^SPX"
 
-dax_data = StooqDailyReader(sp500).read()
+dax_data = StooqDailyReader(dax).read()['Close']
+mwig40_data = StooqDailyReader(wig40).read()['Close']
+spx_data = StooqDailyReader(sp500).read()['Close']
 
 # variance
+print('dax')
 print(bollinger(dax_data, 50, 0))
-print(bollinger(dax_data, 50, 1))
-print(bollinger(dax_data, 50, 2))
+
+print('mwig40')
+print(bollinger(mwig40_data, 50, 0))
+
+print('sp500')
+print(bollinger(spx_data, 50, 0))
